ee7e4de1cdf4d5755cc06825ebbb4767733e3393
Atya <nushnushka@gmail.com>
2022-03-07 13:33:26 -0300

    views

--- app/views.py
+++ app/views.py
@@ -9,14 +9,19 @@ from app.schema import USER_CREATE, AD_CREATE
 # Должны быть реализованы методы создания/удаления/редактирования объявления
 # POST метод должен создавать объявление, GET - получать объявление, DELETE - удалять объявление.
 
+@app.route("/hello")
+def api_hello_world():
+    return jsonify({"Hello": "world"})
+
+
 class UserView(MethodView):
 
-    def get_user(self, user_id):
+    def get(self, user_id):
         user = User.by_id(user_id)
         return jsonify(user.to_dict())
 
     @validate('json', USER_CREATE)
-    def post_user(self):
+    def post(self):
         user = User(**request.json)
         user.set_password(request.json['password'])
         user.add()
@@ -25,25 +30,25 @@ class UserView(MethodView):
 
 class AdView(MethodView):
 
-    def get_ad(self, ad_id):
+    def get(self, ad_id):
         ad = Ad.by_id(ad_id)
         return jsonify(ad.to_dict())
 
     @validate('json', AD_CREATE)
-    def post_ad(self):
+    def post(self):
         ad = Ad(**request.json)
         ad.add()
         return jsonify(ad.to_dict())
 
-    def delete_ad(self, ad_id):
+    def delete(self, ad_id):
         ad = Ad.by_id(ad_id)
         ad.delete()
         return jsonify({'message': f'Ad was deleted'})
 
 
-app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('users_get'), methods=['GET', ])
-app.add_url_rule('/users/', view_func=UserView.as_view('users_create'), methods=['POST', ])
+app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('users_get'))
+app.add_url_rule('/users', view_func=UserView.as_view('users_create'))
 
-app.add_url_rule('/ads/', view_func=AdView.as_view('ads_create'), methods=['POST', ])
-app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('ads_get'), methods=['GET', ])
-app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('ads_delete'), methods=['DELETE', ])
+app.add_url_rule('/ads', view_func=AdView.as_view('ads_create'))
+app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('get_ad'))
+app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('ads_delete'))
